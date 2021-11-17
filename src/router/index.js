239a5c7512670e170d60a60e1f7297be7070129c
jsponce <jonny47169@gmail.com>
2021-11-17 17:57:20 -0500

    cambios

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,24 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/panel',
+    component: () => import(/* webpackChunkName: "about" */ '../views/panel/Template'),
+    children:[
+      {
+        path: 'inicio',
+        name:'panelInicio',
+        component: () => import(/* webpackChunkName: "about" */ '../views/panel/inicio/Index'),
+      },
+      {
+        path: 'usuarios',
+        name:'panelUsuarios',
+        component: () => import(/* webpackChunkName: "about" */ '../views/panel/usuarios/Index'),
+      }
+    ]
   }
 ]
 
---
+++ src/views/panel/inicio/Index.vue
@@ -0,0 +1,40 @@
+<template>
+<div>
+    <div class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-12">
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item">
+                            <router-link  :to="{name:'panelInicio'}">INICIO</router-link>
+                        </li>
+                    </ol>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h3 class="card-title">PANEL DE BIENVENIDA</h3>
+
+                        <div class="card-tools">
+                            <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
+                                <i class="fas fa-minus"></i>
+                            </button>
+                        </div>
+                    </div>
+                    <div class="card-body" style="display: block;">
+                        You can Change Pace Styles, Checkout the <a href="https://adminlte.io/docs/3.1/" target="_blank" rel="noopener noreferrer">AdminLTE Official Docs</a> in Online.
+                        <br>
+                        Start creating your amazing application!
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</template>
